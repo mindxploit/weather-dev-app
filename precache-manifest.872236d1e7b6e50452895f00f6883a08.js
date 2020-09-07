ae11c983732aa12627861452a515f8d691345a6e
mindxploit <bobbonicola@gmail.com>
2020-09-07 17:02:52 +0200

    Updates

---
+++ precache-manifest.872236d1e7b6e50452895f00f6883a08.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6c4d10fc5dec9f1f7a50ed2c06969e21",
+    "url": "/weather-dev-app/index.html"
+  },
+  {
+    "revision": "e7ff6e1ee963dddb303e",
+    "url": "/weather-dev-app/static/js/2.2f2cf4b0.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/weather-dev-app/static/js/2.2f2cf4b0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b113c02d4c1b8030f10e",
+    "url": "/weather-dev-app/static/js/main.ffaf89aa.chunk.js"
+  },
+  {
+    "revision": "0186a0836100269b5b88",
+    "url": "/weather-dev-app/static/js/runtime-main.af09e631.js"
+  },
+  {
+    "revision": "a8528a868d461f09ee29a47e4c5323c8",
+    "url": "/weather-dev-app/static/media/Clear.a8528a86.png"
+  },
+  {
+    "revision": "11452cab98ccf2a171a36da25360ba1b",
+    "url": "/weather-dev-app/static/media/Cloud-background.11452cab.png"
+  },
+  {
+    "revision": "c6d1cb3616b92442a99eb3acd4063a89",
+    "url": "/weather-dev-app/static/media/LightCloud.c6d1cb36.png"
+  },
+  {
+    "revision": "b62e950be97488071f86695792d1a919",
+    "url": "/weather-dev-app/static/media/Showers.b62e950b.png"
+  },
+  {
+    "revision": "e1202bb6505e7b4c7a9fac14eafe4162",
+    "url": "/weather-dev-app/static/media/Sleet.e1202bb6.png"
+  }
+]);
